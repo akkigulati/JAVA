b3982302df1932f74a1316b84c4e793f62784788
akkigulati <41364230+akkigulati@users.noreply.github.com>
2020-05-24 15:48:31 +0530

    Update maximumNonOverlappingBridges.java

--- DP/maximumNonOverlappingBridges.java
+++ DP/maximumNonOverlappingBridges.java
@@ -62,6 +62,9 @@ public class Main {
         int[] dp = new int[n];
         //  dp[0]=1
         int ans=Integer.MIN_VALUE;
+      
+      //appling longest common subsequence..
+      
         for (int i = 0; i < n; i++) {
             int max = 0;
             for (int j = 0; j < i; j++) {
