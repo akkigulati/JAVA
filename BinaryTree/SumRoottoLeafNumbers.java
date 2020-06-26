ad938c3834138cd4ce1d4b150cd93b44b17f81b4
akkigulati <41364230+akkigulati@users.noreply.github.com>
2020-06-26 13:43:07 +0530

    Update SumRoottoLeafNumbers.java

--- BinaryTree/SumRoottoLeafNumbers.java
+++ BinaryTree/SumRoottoLeafNumbers.java
@@ -34,6 +34,25 @@ The root-to-leaf path 4->0 represents the number 40.
 Therefore, sum = 495 + 491 + 40 = 1026.
 */
 class Solution {
+    public int sumNumbers(TreeNode root) {
+        if (root == null) return 0;
+        int[] res = new int[]{0};
+        dfs(root, res, 0);
+        return res[0];
+    }
+    
+    private void dfs(TreeNode root, int[] res, int cur){
+        if (root == null) return;
+        if (root.left == null && root.right == null){
+            res[0] += cur * 10 + root.val;
+            return;
+        }
+        dfs(root.left, res, 10 * cur + root.val);
+        dfs(root.right, res, 10 * cur + root.val);
+    }
+}
+///////////////////////////////////////////////////////
+class Solution {
     
     public int sumNumbers(TreeNode root) {
         ArrayList<String> ans=new ArrayList<>();
