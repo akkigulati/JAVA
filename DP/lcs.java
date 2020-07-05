bc7b04ea3492bab4fc9d12ec573a2ea44e335581
akkigulati <41364230+akkigulati@users.noreply.github.com>
2020-07-05 19:25:22 +0530

    Update lcs.java

--- DP/lcs.java
+++ DP/lcs.java
@@ -37,3 +37,29 @@ public static void main(String[] args) {
      return mem[n][m];
  }
 }
+/*
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) {
+
+    Scanner sc=new Scanner(System.in);
+    String s1=sc.nextLine();
+    String s2=sc.nextLine();
+    int dp[][]=new int[s1.length()+1][s2.length()+1];
+    //tabulation
+    for(int i=1;i<=s1.length();i++){
+        for(int j=1;j<=s2.length();j++){
+            if(s1.charAt(i-1)==s2.charAt(j-1)){
+                dp[i][j]=dp[i-1][j-1]+1;
+            }else{
+                dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+    }
+    System.out.println(dp[s1.length()][s2.length()]);
+ 
+}
+}
+*/
