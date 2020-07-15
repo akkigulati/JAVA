1778503a4ed60e932afa20c2ccf70e6580352b4b
akkigulati <41364230+akkigulati@users.noreply.github.com>
2020-07-15 15:24:50 +0530

    Create longestincreasingSubsequence.java

---
+++ DP/longestincreasingSubsequence.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) {
+
+int arr[]={2,5,3,6};
+
+
+System.out.println(lis(arr));
+ }
+ public static int lis(int arr[]){
+     int n=arr.length;
+     int lis[]=new int[n];
+     lis[0]=1;
+    for(int i=1;i<n;i++){
+        lis[i]=1;
+        
+        for(int j=0;j<i;j++){
+            if(arr[j]<arr[i]){
+                lis[i]=Math.max(lis[i],lis[j]+1);
+            }
+        }
+        
+    }
+    int ans=lis[0];
+    for(int i=1;i<n;i++){
+        ans=Math.max(ans,lis[i]);
+    } 
+    return ans;
+ }
+}
