f96fccde030484fe0a876fa1c98089a1174ab120
akkigulati <41364230+akkigulati@users.noreply.github.com>
2020-07-24 17:04:44 +0530

    Update climbStairsWithJumps.java

--- DP/climbStairsWithJumps.java
+++ DP/climbStairsWithJumps.java
@@ -1,6 +1,83 @@
 import java.io.*;
 import java.util.*;
+/*recursive
+import java.io.*;
+import java.util.*;
 
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] jumps = new int[n];
+        for (int i = 0; i < n; i++) {
+            jumps[i] = sc.nextInt();
+        }
+
+        System.out.println(climb(0, jumps));
+
+    }
+    public static int climb(int idx, int jumps[]) {
+        if (idx == jumps.length-1) {
+            return 1;
+        }
+        int totalways=0;
+        int maxJumps = jumps[idx];
+       
+            for (int i = 1; i <= maxJumps; i++) {
+                if (i + idx < jumps.length)
+                    totalways += climb(idx + i, jumps);
+            }
+        
+        
+        return totalways;
+    }
+
+}
+*/
+/*
+memorization
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] jumps = new int[n];
+        for (int i = 0; i < n; i++) {
+            jumps[i] = sc.nextInt();
+        }
+
+        System.out.println(climb(0, jumps,new int[n]));
+
+    }
+    public static int climb(int idx, int jumps[],int dp[]) {
+        if (idx == jumps.length-1) {
+            dp[idx]=1;
+            return 1;
+        }
+        if(dp[idx]!=0){
+            return dp[idx];
+        }
+        int totalways=0;
+        int maxJumps = jumps[idx];
+       
+            for (int i = 1; i <= maxJumps; i++) {
+                if (i + idx < jumps.length)
+                    totalways += climb(idx + i, jumps,dp);
+            }
+        
+        
+        return dp[idx]=totalways;
+
+
+
+    }
+
+}
+*/
 public class Main {
 
     public static void main(String[] args) throws Exception {
