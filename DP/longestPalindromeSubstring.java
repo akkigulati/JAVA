7a00e82e232f736c14d8bfcc37467354546e69e4
akkigulati <41364230+akkigulati@users.noreply.github.com>
2020-07-26 20:37:29 +0530

    Update longestPalindromeSubstring.java

--- DP/longestPalindromeSubstring.java
+++ DP/longestPalindromeSubstring.java
@@ -14,7 +14,40 @@ Sample Output
 4
 
 */
+import java.io.*;
+import java.util.*;
 
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        int n1 = s1.length();
+        int dp[][] = new int[n1][n1];
+        for (int dia = 0; dia < n1; dia++) {
+            for (int i = 0, j = dia; j < n1; j++, i++) {
+                if (dia == 0) {
+                    dp[i][j] = 1;
+                } else if (dia == 1) {
+                    if (s1.charAt(i)==s1.charAt(j)){
+                        dp[i][j]=2;
+                    }else{
+                        dp[i][j]=1;
+                    }
+                }else{
+                    if (s1.charAt(i)==s1.charAt(j)){
+                        dp[i][j]=dp[i+1][j-1]+2;
+                    }else{
+                        dp[i][j]=Math.max(dp[i][j-1],dp[i+1][j]);
+                    }
+                }
+            }
+        }
+        System.out.println(dp[0][n1-1]);
+    }
+
+}
+////////////////////////////////////////////////////////////////////////
 import java.io.*;
 import java.util.*;
 
