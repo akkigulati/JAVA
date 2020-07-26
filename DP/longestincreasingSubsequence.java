18ed6042f2d6691de42bf4352654c69402936b56
akkigulati <41364230+akkigulati@users.noreply.github.com>
2020-07-26 14:20:14 +0530

    Update longestincreasingSubsequence.java

--- DP/longestincreasingSubsequence.java
+++ DP/longestincreasingSubsequence.java
@@ -1,3 +1,61 @@
+/*
+Question
+1. You are given a number n, representing the number of elements.
+2. You are given n numbers, representing the contents of array of length n.
+3. You are required to print the length of longest increasing subsequence of array.
+Input Format
+A number n
+.. n more elements
+Output Format
+A number representing the length of longest increasing subsequence of array.
+Constraints
+0 <= n <= 20
+0 <= n1, n2, .. <= 100
+Sample Input
+1
+64
+
+Sample Output
+1
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int lis[]=new int[n];
+        lis[0]=1;
+        for(int i=1;i<n;i++){
+            int temp=0;
+            for(int j=0;j<i;j++){
+                if(arr[i]>arr[j]&&temp<lis[j]){
+                    temp=lis[j];
+                }
+            }
+            lis[i]=temp+1;
+        }
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            max=Math.max(max,lis[i]);
+        }
+        System.out.println(max);
+        
+    }
+
+}
+
+
+
+
+///////////////
 import java.util.*;
 
 public class Main{
